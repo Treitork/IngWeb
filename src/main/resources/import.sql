6703a29629eca7e30d3cdf2734455d4e5e8e88a7
Sebas <saguila@ucm.es>
2015-11-09 18:11:46 +0100

    dsf

---
+++ src/main/resources/import.sql
@@ -0,0 +1,18 @@
+insert into User values (0, '$2a$10$08Q7cGYjNw2nLcmblfpOdeF5tbO2H04W3vF86v/1OOmU6ojKhHc/K', 'admin', 'admin');
+insert into User values (1, '$2a$10$ld2WZ99kqW8C5x7cbnCzteYkGzN62waU.zeSvHeBgt2sa30gHnrZq', 'user', 'user');
+insert into User values (2, '$2a$10$KdoEy6tQTbrJ.pRx/oteYubJU/90fkSsvIiIpQoDGH..lvzLgDOly', 'test', 'user');
+insert INTO AUTHOR VALUES(1,'Adams','Adam')
+insert INTO AUTHOR VALUES(2,'Bobbie','Bob')
+insert INTO AUTHOR VALUES(3,'Carlos','Carl')
+INSERT INTO AUTHOR VALUES(4,'Dieguez','Diego')
+INSERT INTO BOOK VALUES(1,'book one','one', 1)
+INSERT INTO BOOK VALUES(2,'book two','two', 1)
+INSERT INTO BOOK VALUES(3,'book three','three', 2)
+INSERT INTO BOOK VALUES(4,'book four','four', 2)
+INSERT INTO BOOK VALUES(5,'book five','five', 2)
+INSERT INTO AUTHOR_WRITINGS VALUES(1,1)
+INSERT INTO AUTHOR_WRITINGS VALUES(1,2)
+INSERT INTO AUTHOR_WRITINGS VALUES(1,3)
+INSERT INTO AUTHOR_WRITINGS VALUES(1,4)
+INSERT INTO AUTHOR_WRITINGS VALUES(2,1)
+INSERT INTO AUTHOR_WRITINGS VALUES(3,1)
