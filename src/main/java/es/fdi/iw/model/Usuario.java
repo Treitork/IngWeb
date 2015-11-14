a7210631b0ac042220168626dc72d73824e489f7
Sebas <saguila@ucm.es>
2015-11-14 18:38:23 +0100

    añadida contraseña.

--- src/main/java/es/fdi/iw/model/Usuario.java
+++ src/main/java/es/fdi/iw/model/Usuario.java
@@ -2,7 +2,23 @@ package es.fdi.iw.model;
 
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Entity
+@NamedQueries({
+@NamedQuery(name="allUsers",
+query="select u from Usuario u"),
+@NamedQuery(name="userByLogin",
+query="select u from Usuario u where u.contrasenia = :loginParam")
+})
+
 public class Usuario {
+private static BCryptPasswordEncoder bcryptEncoder = new BCryptPasswordEncoder();
+private String hashedAndSalted;
 private boolean activo;
 private String nombre,apellidos,contrasenia,imagen,rol;
 private Integer puntuacion;
@@ -10,6 +26,7 @@ private List<Asignatura> clases;
 private List<Votacion> recibidas;
 private List<HistorialVotaciones> historial;
 
+public Usuario(){}
 public Usuario(String nombre, String apellidos, String contrasenia, String rol) {
 	this.nombre = nombre;
 	this.apellidos = apellidos;
@@ -19,6 +36,23 @@ public Usuario(String nombre, String apellidos, String contrasenia, String rol) 
 	this.recibidas = null;
 	this.historial = null;
 	this.activo = true;
+}
+
+public static Usuario crearUsuario(String nombre, String apellidos, String contrasenia, String rol){
+	Usuario u = new Usuario();
+	u.nombre = nombre;
+	u.apellidos = apellidos;
+	u.hashedAndSalted = bcryptEncoder.encode(contrasenia);
+	u.rol = rol;
+	u.clases = null;
+	u.recibidas = null;
+	u.historial = null;
+	u.activo = true;
+	return u;
+}
+
+public boolean esValidaContrasenia(String contrasenia) {
+	return bcryptEncoder.matches(contrasenia, hashedAndSalted);		
 }
 
 /** añade asignatura al usuario */
