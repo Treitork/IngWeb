67817b4e88e1b64c208d67cae880d1ac879800e4
eagle <eagle@eagle-HP-Pavilion-17-Notebook-PC>
2015-12-05 21:05:25 +0100

    En leeme esta el orden a la hora de importar en la base de datos. Funcionando Correctamente JPA

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,1 @@
-insert into Usuario values (1,'email@email.com','$2a$10$ld2WZ99kqW8C5x7cbnCzteYkGzN62waU.zeSvHeBgt2sa30gHnrZq','nombre','apellidos', 'usuario',9,false,'imagen');
+insert into Usuario values (1,TRUE,'Aguila','saguila@ucm.es','$2a$10$ld2WZ99kqW8C5x7cbnCzteYkGzN62waU.zeSvHeBgt2sa30gHnrZq','no-image','Sebastian',0,'user');
